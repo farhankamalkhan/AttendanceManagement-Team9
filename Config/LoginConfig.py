5d95417e15262a1d14edce1878625640f460a731
Farhan Kamal Khan <farhankamalkhan@gmail.com>
2018-10-27 12:46:30 +0500

    Add attendance management basic flow
    Signed-off-by: Farhan Kamal Khan <farhankamalkhan@gmail.com>

---
+++ Config/LoginConfig.py
@@ -0,0 +1,10 @@
+config = {
+    "URL":  "http://10.0.0.165:8080/#/home",
+    "browser": "Firefox",
+    "name": "uaefhjdtgryibck atcubdierjfghyk",
+    "userName": "league9",
+    "pass": "10pearls1+",
+    "speed": "1",
+    "timeout": "60s",
+    "timeout2": "80s"
+    }
